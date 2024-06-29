68293efec164e755da079f85cf5250f44c6b51f5
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-06-29 23:42:32 +0900

    fix: ログが2重で出力される不具合を修正

--- src/coop_kobe_parser/logger.py
+++ src/coop_kobe_parser/logger.py
@@ -8,18 +8,15 @@ def init_logger():
     Returns:
       logging.Logger: The logger object.
     """
-
     # Create a logger object.
     logger = logging.getLogger(__name__)
 
-    # Set the log level for the logger object.
-    logger.setLevel(logging.INFO)
-
-    # Create a handler that outputs logs to the console.
-    console_handler = logging.StreamHandler()
-
-    # Add the console handler to the logger object.
-    logger.addHandler(console_handler)
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
 
     # Return the logger object.
     return logger
