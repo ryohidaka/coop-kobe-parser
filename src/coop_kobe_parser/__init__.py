8faa9098572de6caa36aee4613a98a2baeabe370
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-06-29 23:43:14 +0900

    refact: ログ出力を追加

--- src/coop_kobe_parser/__init__.py
+++ src/coop_kobe_parser/__init__.py
@@ -17,6 +17,7 @@ def main():
 
     # 解析する CSV ファイルのパス
     csv_path = "demo/demo.csv"
+    parser.logger.info(f"解析を開始します: {csv_path}")
 
     # CSV ファイルを解析
     data = parser.parse(csv_path)
@@ -29,6 +30,7 @@ class CoopKobeParser:
         CoopKobeParser クラスの初期化。
         """
         self.logger = init_logger()
+        self.logger.info("CoopKobeParser クラスを初期化しました。")
 
     def parse(self, csv_path: str) -> dict:
         """
@@ -40,17 +42,25 @@ class CoopKobeParser:
         Returns:
         dict: パース結果
         """
+        self.logger.info(f"CSV ファイルの解析を開始します: {csv_path}")
+
         # CSV ファイルを読み込み
         df = self._load_csv(csv_path)
+        if df is None:
+            self.logger.error("CSV ファイルの読み込みに失敗しました。")
+            return {}
 
         # 商品一覧と支払情報のデータフレームに分ける
         df_products, df_summary = self._divide_df(df)
+        self.logger.info("データフレームの分割が完了しました。")
 
         # 商品一覧をパースして配列化する
         products = ProductParser(df_products).parse()
+        self.logger.info("商品一覧のパースが完了しました。")
 
         # 支払情報をパースする
         summary = SummaryParser(df_summary).parse()
+        self.logger.info("支払情報のパースが完了しました。")
 
         return {"products": products, "summary": summary}
 
@@ -65,8 +75,10 @@ class CoopKobeParser:
         DataFrame: CSV ファイルから読み込んだデータフレーム
         """
         try:
+            self.logger.info(f"CSV ファイルを読み込みます: {path}")
             # CSV ファイルを読み込み
             df = read_csv(path, dtype=str)
+            self.logger.info("CSV ファイルの読み込みが成功しました。")
             return df
         except FileNotFoundError:
             self.logger.error(f"ファイル {path} が見つかりませんでした。")
@@ -87,7 +99,10 @@ class CoopKobeParser:
 
         # 品番が存在するかどうかを確認
         if "品番" not in df.columns:
+            self.logger.error("品番列がデータフレームに存在しません。")
             raise ValueError("品番列がデータフレームに存在しません。")
+
+        self.logger.info("品番列の確認が完了しました。")
 
         # 商品一覧のデータフレームを取得 (品番がNaNでない)
         df_products = df[df["品番"].notna()]
@@ -97,6 +112,8 @@ class CoopKobeParser:
 
         # 商品一覧と支払情報のデータフレームが空でないことを確認
         if df_products.empty or df_summary.empty:
+            self.logger.error("商品一覧または支払情報のデータフレームが空です。")
             raise ValueError("商品一覧または支払情報のデータフレームが空です。")
 
+        self.logger.info("データフレームの分割に成功しました。")
         return df_products, df_summary
